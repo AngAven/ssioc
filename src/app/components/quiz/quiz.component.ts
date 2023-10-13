294dfc4a99ed72aff140266720b9a20b710a1080
Angel <angel.aven.c@gmail.com>
2023-10-13 17:02:31 -0600

    Primera versión con elementos pseudofinales

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 
 import {StoreService} from "../../services/store.service";
 import {QuizService} from "../../services/quiz.service";
+import {UserDTO} from "../../models/auth.model";
 
 @Component({
   selector: 'app-quiz',
@@ -11,7 +12,12 @@ import {QuizService} from "../../services/quiz.service";
 export class QuizComponent implements OnInit {
   quizNumber: number = 0
   quizPeriod: string = ''
-  quiz: any = {}
+  quizResponse: any = {}
+  userData: UserDTO = {}
+
+  // replace
+  titulo: string = ''
+  preguntas: any = ''
 
   constructor(
     private storeService: StoreService,
@@ -27,9 +33,31 @@ export class QuizComponent implements OnInit {
       }
     })
 
+    this.storeService.user$.subscribe(userDTO => {
+      this.userData = userDTO
+    })
+
     this.quizService.getQuiz(`formato${this.quizNumber}`, this.quizPeriod)
       .subscribe(data => {
-        this.quiz = data
+        this.quizResponse = data
+
+        let quizName = ''
+        if(this.quizPeriod !== ''){
+          switch (this.quizPeriod) {
+            case 'Enero':
+              quizName = `formato${this.quizNumber}a`
+              break
+            case 'Marzo':
+              quizName = `formato${this.quizNumber}b`
+              break
+            default:
+              alert('no se encontro un formato')
+          }
+        }
+
+        //replace
+        this.titulo = this.quizResponse?.nombreFormato
+        this.preguntas = this.quizResponse[quizName]
       })
   }
 }
