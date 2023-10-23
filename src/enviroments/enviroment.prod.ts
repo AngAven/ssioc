c570bbde82bbda44978f6cc1bd188c8219fe7dc6
Angel <angel.aven.c@gmail.com>
2023-10-23 14:09:40 -0600

    Configuración de variables de producción para despliegue en ambientes

--- src/enviroments/enviroment.prod.ts
+++ src/enviroments/enviroment.prod.ts
@@ -1,7 +1,7 @@
-export const environment = {
-  production: false,
+export const staging = {
+  production: true,
   API_URL: 'http://10.35.16.9:8090/SSIOC-presentacion-1.0.0',
   API_URL_MAIL: 'http://10.35.16.9:8090/SSIOC-presentacion-1.0.0/views/enviaCorreo',
-  ASSETS_LOCATION: './assets',
+  ASSETS_LOCATION: '/SSIOC-presentacion-1.0.0/views/dist/assets',
   API_GOOGLE_SCRIPTS_FORMATO_1A_MARZO: 'https://script.google.com/macros/s',
 };
