7e97a37ea3b1877a486b08851a397196eb835bdc
Angel <angel.aven.c@gmail.com>
2023-10-31 00:14:33 -0600

    Se adecuan las variables de producción y staging

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_SSIOC: 'http://10.35.16.9:8090/SSIOC-presentacion-1.0.0',
+  API_SSIOC: '/SSIOC-presentacion-1.0.0',
   ASSETS_LOCATION: '/SSIOC-presentacion-1.0.0/views/dist/assets',
   API_GOOGLE_RECAPTCHA: '6LdtuZcUAAAAAB3uKODkzd-zIu9hIxxU-Icj_6bx',
   API_GOOGLE_SCRIPTS: 'https://script.google.com/macros/s',
