33d544af07f567ad3f0c27c1a57449504f395c86
Angel <angel.aven.c@gmail.com>
2023-11-13 17:19:28 -0600

    Configuración del build para la versión v2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  API_SSIOC: '/SSIOC-presentacion-1.0.0',
-  ASSETS_LOCATION: '/SSIOC-presentacion-1.0.0/views/dist/assets',
+  API_SSIOC: '/SSIOC-presentacion-2.0.0',
+  ASSETS_LOCATION: '/SSIOC-presentacion-2.0.0/views/dist/assets',
   API_GOOGLE_RECAPTCHA: '6LdtuZcUAAAAAB3uKODkzd-zIu9hIxxU-Icj_6bx',
   API_GOOGLE_SCRIPTS: 'https://script.google.com/macros/s',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  API_SSIOC: 'http://10.35.16.9:8090/SSIOC-presentacion-1.0.0',
+  API_SSIOC: 'http://10.35.16.9:8090/SSIOC-presentacion-2.0.0',
   ASSETS_LOCATION: './assets',
   API_GOOGLE_RECAPTCHA: '6LdtuZcUAAAAAB3uKODkzd-zIu9hIxxU-Icj_6bx',
   API_GOOGLE_SCRIPTS: 'https://script.google.com/macros/s',
